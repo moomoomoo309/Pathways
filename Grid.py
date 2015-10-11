e941a281fbafacb75e2fd77cdeff32af18b0e42d
nicky <nicky@nicky-HP-Compaq-dc7900-Small-Form-Factor>
2015-10-11 12:43:12 -0400

    Made the grid work like pathfinding.js does. Does not use buttons, using just the canvas, allowing for more robust control over the grid, albeit with some more work involved.

--- Grid.py
+++ Grid.py
@@ -5,35 +5,80 @@ from kivy.core.window import Window
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.behaviors import DragBehavior
 from kivy.input.motionevent import MotionEvent
+from kivy.uix.widget import Widget
+from kivy.graphics import *
+from math import ceil
+from collections import namedtuple
 
-squareSize=40
+squareSize=32
+marginSize=1
 buttonList=[]
-print(Window.width)
+Point=namedtuple("Point", "x y")
+lastButton={"pos":(0,0),"size":(0,0),"colored":False}
+lastColor=True
+   
 
-def updateLastButton(self):
-    lastButton=self
-    
-def findButton(pos):
+def findButton(pos,allowRepeats):
+    x=pos[0]
+    y=pos[1]
+    global lastButton
     for i in buttonList:
-        if i.collide_point(pos):
+        if i["pos"].x<=x<=i["pos"].x+i["size"].x and i["pos"].y<=y<=i["pos"].y+i["size"].y and (i!=lastButton or allowRepeats):
+            lastButton=i
             return i
-    
-class Grid(App):
-    def build(self):
-        layout=GridLayout(cols=Window.width/squareSize)
-        for x in range(0,Window.width/squareSize):
-            for y in range(0,Window.height/squareSize):
-                buttonList.append(ToggleButton(on_press=updateLastButton))
-        for i in (buttonList):
-            layout.add_widget(i)
-        return layout
+        
+class GridWidget(Widget):
+    def __init__(self,**kwargs):
+        super(GridWidget,self).__init__(**kwargs)
+        for x in range(0,int(ceil(Window.width/squareSize))+1):
+            for y in range(0,int(ceil(Window.height/squareSize))+1):
+                with self.canvas:
+                    Color(1,1,1)
+                    buttonList.append({"pos":Point(x*(squareSize+marginSize),y*(squareSize+marginSize)),"size":Point(squareSize+marginSize,squareSize+marginSize),"colored":False})
+                    Rectangle(pos=(x*(squareSize+marginSize),y*(squareSize+marginSize)),size=(squareSize+marginSize,squareSize+marginSize))
+                    Color(0,0,0)
+                    Rectangle(pos=(x*(squareSize+marginSize),y*(squareSize+marginSize)),size=(squareSize,squareSize))
 
-#Does not work yet, but the idea is to make them toggle on drag like pathfinding.js.   
+    def on_touch_down(self, touch):
+        global lastButton
+        global lastColor
+        foundButton=findButton(touch.pos,True)
+        if foundButton is not None:
+            foundButton["colored"]=not foundButton["colored"]
+            lastColor=foundButton["colored"]
+            with self.canvas:
+                if foundButton["colored"]:
+                    Color(.5,.5,.5)
+                else:
+                    Color(1,1,1)
+                Rectangle(pos=(foundButton["pos"].x,foundButton["pos"].y),size=(foundButton["size"].x,foundButton["size"].y))
+                if foundButton["colored"]:
+                    Color(.25,.25,.25)
+                else:
+                    Color(0,0,0)
+                Rectangle(pos=(foundButton["pos"].x,foundButton["pos"].y),size=(foundButton["size"].x-marginSize,foundButton["size"].y-marginSize))
+
+        
     def on_touch_move(self, touch):
-        if findButton(pos=touch.pos)!=lastButton:
-            findButton(pos=touch.pos).toggle()
-            return true
+        global lastButton
+        foundButton=findButton(touch.pos,False)
+        if foundButton is not None:
+            foundButton["colored"]=lastColor
+            with self.canvas:
+                if foundButton["colored"]:
+                    Color(.5,.5,.5)
+                else:
+                    Color(1,1,1)
+                Rectangle(pos=(foundButton["pos"].x,foundButton["pos"].y),size=(foundButton["size"].x,foundButton["size"].y))
+                if foundButton["colored"]:
+                    Color(.25,.25,.25)
+                else:
+                    Color(0,0,0)
+                Rectangle(pos=(foundButton["pos"].x,foundButton["pos"].y),size=(foundButton["size"].x-marginSize,foundButton["size"].y-marginSize))
         
-        
+class OtherGrid(App):
+    def build(self):
+        return GridWidget()
+
 if __name__== "__main__":
-    Grid().run()
+    OtherGrid().run()
