5889585152ab65b06e1752bcaffdac740c82d35e
nicky <nicholas_delello@yahoo.com>
2015-10-26 18:49:28 -0400

    Removed HTTP verification because it takes too long.
    Also changed image URLS.

--- Calendar.py
+++ Calendar.py
@@ -3,32 +3,33 @@ from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
-from kivy.graphics.vertex_instructions import Rectangle
 from kivy.uix.label import Label
 from kivy.uix.image import AsyncImage
 from random import randint
-from os.path import isfile
-from kivy.network.urlrequest import UrlRequest
 from datetime import date
 
+from kivy.graphics.vertex_instructions import Rectangle
+
 topBarSize = 75
-# The size of the top bar
+#The size of the top bar
 CalWidget = None
-CurrentMonth = date.today().month-1 #It's table indices (0-11), not month count (1-12)
-Images = ["//hs4.hs.ptschools.org/data_student$/2016/My_Documents/1009877/Documents/My Pictures/38e.png",
-          "//hs4.hs.ptschools.org/data_student$/2016/My_Documents/1009877/Documents/My Pictures/simple_skeleton.png",
-          "//hs4.hs.ptschools.org/data_student$/2016/My_Documents/1009877/Documents/My Pictures/RainbowPenguins.jpg",
-          "http://icons.iconseeker.com/png/fullsize/creeps/skeleton-1.png"] #Replace these with pictures of your choice.
+#The calendar widget object, so it can be referenced on resize.
+randomImages = True
+CurrentMonth = date.today().month - 1  #It's table indices (0-11), not month count (1-12)
+Images = {9: ["http://images2.wikia.nocookie.net/__cb20120728022911/monsterhigh/images/1/1d/Skeletons.jpg",
+              "https://images.duckduckgo.com/iu/?u=http%3A%2F%2Fimages.fineartamerica.com%2Fimages-medium-large-5%2Fdancing-skeletons-liam-liberty.jpg&f=1",
+              "http://ih1.redbubble.net/image.24320851.9301/flat,550x550,075,f.jpg",
+              "http://icons.iconseeker.com/png/fullsize/creeps/skeleton-1.png"]}  #Replace these with pictures of your choice
 
 Months = {"January": 31, "February": 28, "March": 31, "April": 30, "May": 31, "June": 30, "July": 31, "August": 31,
-                                                "September": 31, "October": 31, "November": 30, "December": 31}
-MonthNames=["January","February","March","April","May","June","July","August","September","October","November","December"]
+          "September": 31, "October": 31, "November": 30, "December": 31}
+MonthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+              "November", "December"]
+
 
 def getImageSource():
-    for i in range(0, 2 * Images.__len__()):
-        img = Images[randint(0, Images.__len__() - 1)]
-        if isfile(img) or (img[0:3] == "http" and UrlRequest(url=img, wait=.1).resp_status == 200):
-            return img
+    if randomImages:
+        return Images[CurrentMonth][randint(0, Images[CurrentMonth].__len__() - 1)]
     return "CalendarInactive.png"
 
 
@@ -50,10 +51,11 @@ class CalendarGrid(GridLayout):
         self.pos[0] += 1
         self.spacing = 1
         # Center it a little nicer than kivy does by default.
-        gridSize=(Window.width / 7, (Window.height - topBarSize) / 6)
+        gridSize = (Window.width / 7, (Window.height - topBarSize) / 6)
         #The size of each box in the grid
         for i in range(0, MonthStart):
-            self.add_widget(AsyncImage(source=getImageSource(),allow_stretch=True,keep_ratio=False,size=gridSize))  # If the month doesn't start on a Monday, you need an empty day.
+            self.add_widget(AsyncImage(source=getImageSource(), allow_stretch=True, keep_ratio=False,
+                                       size=gridSize))  # If the month doesn't start on a Monday, you need an empty day.
         for i in range(0, MonthLength):
             self.add_widget(ToggleButton(texture=None, background_normal="CalendarInactive.png",
                                          background_down="CalendarActive.png", group="CalendarGrid_Days",
@@ -62,13 +64,13 @@ class CalendarGrid(GridLayout):
                                          valign="top", text_size=gridSize))
             # The group means they act as radio buttons, so only one is toggleable at a time.
         for i in range(0, self.rows * self.cols - MonthLength - MonthStart):
-            self.add_widget(AsyncImage(source=getImageSource(),allow_stretch=True,keep_ratio=False,size=gridSize))
+            self.add_widget(AsyncImage(source=getImageSource(), allow_stretch=True, keep_ratio=False, size=gridSize))
 
 
-def redraw(Window, width, height):
+def redraw(*args):
     global CalWidget
     CalWidget.canvas.clear()
-    CalWidget.__init__(Month=CurrentMonth)
+    CalWidget.__init__(Month=MonthNames[CurrentMonth])
     return CalWidget
 
 
@@ -84,7 +86,8 @@ class CalendarWidget(Widget):
                               pos=(0, Window.height - topBarSize), markup=True, halign="center", valign="middle"))
         # It's got markup in it for color and size, and the text is centered vertically and horizontally.
         # The text is from the keyword argument "Month".
-        self.add_widget(CalendarGrid(MonthLength=Months[kwargs["Month"]], MonthStart=date.today().replace(day=1).weekday()))
+        self.add_widget(
+            CalendarGrid(MonthLength=Months[kwargs["Month"]], MonthStart=date.today().replace(day=1).weekday()))
         # And this adds the grid.
 
 
