9fbd1895c167f63aaee757a9c13fd3d0edeec552
Nicholas De Lello <nicholas_delello@yahoo.com>
2016-01-05 13:20:08 -0500

    Make comments consistent.

--- main.py
+++ main.py
@@ -30,16 +30,25 @@ class SettingSlider(SettingItem):
     value = BoundedNumericProperty(min=0, defaultvalue=50)
     def __init__(self, **kwargs):
         super(SettingSlider, self).__init__(**kwargs)
+        # Current value on the slider
         self.value = self.panel.get_value(self.section, self.key)
+
+        # Label with the current value on it
         self.label = Label(text=str(self.value))
+
         self.add_widget(self.label)
-        self.slider = Slider(min=0, max=100, value=self.value, step=1, size_hint_x=5)
         # Slider range is 0-100 with an interval of 1
+        self.slider = Slider(min=0, max=100, value=self.value, step=1, size_hint_x=5)
+
+        # Add the slider in, so it exists
         self.add_widget(self.slider)
-        self.slider.bind(value=lambda self,newVal: setattr(self.parent.parent.parent.label, "text", str(int(newVal))))
+
         # Update the label with the slider value
-        self.label.bind(text=lambda self,newVal: setattr(self.parent.parent.parent, "value", int(newVal)))
+        self.slider.bind(value=lambda self,newVal: setattr(self.parent.parent.parent.label, "text", str(int(newVal))))
+
         # Update the setting from the label value
+        self.label.bind(text=lambda self,newVal: setattr(self.parent.parent.parent, "value", int(newVal)))
+
 
 
 
@@ -48,27 +57,37 @@ class main(App):
     # This is here so things can be placed on the root widget if needed.
     def build(self):
         topBarSize = 75
+        # Put the calendar on the Month view
         app = TabView(size=(Window.width, Window.height), randomImages=True, online=False, topBarSize=topBarSize)
         app.add_screen(makeCalWidget(app))
-        # Put the calendar on the Month view
+
+        # Make screens for the app itself and the settings menu
         appScreen = Screen(name="App")
         settingsScreen = Screen(name="Settings")
-        # Make screens for the app itself and the settings menu
-        settingsScreen.bind(size=partial(genericResize, objs=settingsScreen.children, fct=lambda: Window.size))
+
         # Resize the settings menu on window resize
-        screenManager = ScreenManager(transition=NoTransition(), size=Window.size)
+        settingsScreen.bind(size=partial(genericResize, objs=settingsScreen.children, fct=lambda: Window.size))
+
         # Don't have a transition when switching to the settings menu
-        Window.bind(on_resize=partial(genericResize, objs=screenManager, fct=lambda: Window.size))
+        screenManager = ScreenManager(transition=NoTransition(), size=Window.size)
+
         # Resize all widgets on window resize
-        screenManager.bind(size=partial(genericResize, objs=screenManager.children, fct=lambda: Window.size))
+        Window.bind(on_resize=partial(genericResize, objs=screenManager, fct=lambda: Window.size))
+
         # Propogate resize to all children
+        screenManager.bind(size=partial(genericResize, objs=screenManager.children, fct=lambda: Window.size))
+
+        # Add screens into screen manager
         screenManager.add_widget(appScreen)
         screenManager.add_widget(settingsScreen)
-        # Add screens into screen manager
-        appScreen.add_widget(app)
+
         # Add the app into the screen so it actually shows up
-        appScreen.bind(size=partial(genericResize, objs=app, fct=lambda: Window.size))
+        appScreen.add_widget(app)
+
         # Propogate resize to all children
+        appScreen.bind(size=partial(genericResize, objs=app, fct=lambda: Window.size))
+
+        # Button to go to the settings screen, changes color depending on what's behind it
         settingsButton = Button(pos=(Window.width - topBarSize, Window.height - topBarSize),
                                 size=(topBarSize, topBarSize),
                                 on_press=lambda self: setattr(self.parent.parent, "current",
@@ -77,17 +96,23 @@ class main(App):
                                 background_color=(0, 0, 0, 1) if not shouldUseWhiteText(
                                     app.MonthButton.background_color) else (1, 1, 1, 1), size_hint=(None, None),
                                 mipmap=True)
-        # Button to go to the settings screen, changes color depending on what's behind it
+
+        # Move the settings button so it always stays in the top right corner
         Window.bind(on_resize=lambda self, _, __: setattr(settingsButton, "pos",
                                                           (Window.width - topBarSize, Window.height - topBarSize)))
-        # Move the settings button so it always stays in the top right corner
-        appScreen.add_widget(settingsButton)
+
         # Add the button to switch to the settings screen within the app
+        appScreen.add_widget(settingsButton)
+
+        # Instantiate the settings menu
         settings = Settings()
-        settings.register_type("slider", SettingSlider)
         # Add the slider to the settings menu; kivy does not implement one by default
-        settings.on_close = lambda: setattr(screenManager, "current", screenManager.screens[0].name)
+        settings.register_type("slider", SettingSlider)
+
         # When closing the settings menu, switch back to the app.
+        settings.on_close = lambda: setattr(screenManager, "current", screenManager.screens[0].name)
+
+        # Write defaults to the config if it is missing any fields
         configParser = ConfigParser()
         configParser.read("Settings.cfg")
         configParser.adddefaultsection("Examples")
@@ -97,8 +122,11 @@ class main(App):
         configParser.setdefault("Examples", "List", "Option 1")
         configParser.write()
         configParser.read("Settings.cfg")
-        # Write defaults to the config if it is missing any fields
+
+        # Set up the structure of the actual menu
         settings.add_json_panel("Example settings", configParser, "ExampleSettings.json")
+
+        # Add the settings menu to the screen so it shows up
         settingsScreen.add_widget(settings)
         return screenManager
 
